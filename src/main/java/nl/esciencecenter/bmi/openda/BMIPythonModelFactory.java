098fea90f183c1043db75a55f92f24295aaae135
Niels Drost <n.drost@esciencecenter.nl>
2014-12-16 17:07:42 -0800

    added support for specifying options using arguments in xml file

--- src/main/java/nl/esciencecenter/bmi/openda/BMIPythonModelFactory.java
+++ src/main/java/nl/esciencecenter/bmi/openda/BMIPythonModelFactory.java
@@ -16,6 +16,7 @@
 package nl.esciencecenter.bmi.openda;
 
 import java.io.File;
+import java.util.Arrays;
 
 import nl.esciencecenter.bmi.BMIRaster;
 
@@ -28,28 +29,28 @@ import org.slf4j.LoggerFactory;
 public class BMIPythonModelFactory implements IModelFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BMIPythonModelFactory.class);
-    
+
     private static int nextModelID = 0;
-    
+
     private static int getNextID() {
         int result = nextModelID;
-        
+
         nextModelID += 1;
-        
+
         return result;
     }
-    
+
     private String pythonExecutable;
     private File bridgeDir;
     private File modelDir;
     private String modelModule;
     private String modelClass;
-    
-    private String[] hosts;
-    
+
     private File modelRunRootDir;
-    
+
     private String configFile;
+
+    private String[] hosts;
 
     //called by OpenDA
     public BMIPythonModelFactory() {
@@ -58,34 +59,39 @@ public class BMIPythonModelFactory implements IModelFactory {
 
     @Override
     public void initialize(File configRootDir, String[] arguments) {
-        
-        pythonExecutable = "/home/niels/bin/python";
-        bridgeDir = new File("/home/niels/workspace/eWaterCycle-openda_bmi_python");
-        modelDir = new File("/home/niels/workspace/PCR-GLOBWB/model");
-        modelModule = "bmiPcrglobwb";
-        modelClass = "BmiPCRGlobWB";
-        
+        System.err.println(Arrays.toString(arguments));
         //configFile = "/home/niels/workspace/eWaterCycle-operational/pcrglobwb_config/setup_30min_niels_laptop.ini";
         //configFile = "/home/niels/workspace/eWaterCycle-operational/pcrglobwb_config/setup_RhineMeuse_30arcmin_3layers_ndrost.ini";
-        configFile = arguments[0];
-        
+        //pythonExecutable = "/home/niels/bin/python";
+        //modelClass = "BmiPCRGlobWB";
+        //bridgeDir = new File("/home/niels/workspace/eWaterCycle-openda_bmi_python");
+        //modelDir = new File("/home/niels/workspace/PCR-GLOBWB/model");
+        //modelModule = "bmiPcrglobwb";
+        //modelRunRootDir = new File("/home/niels/Data/operational-output");
+
+        pythonExecutable = arguments[0];
+        bridgeDir = new File(arguments[1]);
+        modelDir = new File(arguments[2]);
+        modelModule = arguments[3];
+        modelClass = arguments[4];
+        modelRunRootDir = new File(arguments[5]);
+        configFile = arguments[6];
+
         if (configFile == null) {
             throw new RuntimeException("Configfile not specified!");
         }
-        
-        
-        //get list of hosts to start models on from environment
-        String hostString = System.getenv("HOSTS");
-        
+
+//        //get list of hosts to start models on from environment
+//        String hostString = System.getenv("HOSTS");
+        String hostString = arguments[7];
+
         if (hostString == null || hostString.isEmpty()) {
-            hosts = new String[] {"localhost"};
+            hosts = new String[] { "localhost" };
         } else {
-            //hosts are seperated by spaces
+            //hosts are separated by spaces
             hosts = hostString.split(" ");
         }
-        
-        
-        modelRunRootDir = new File("/home/niels/Data/operational-output");
+
     }
 
     @Override
@@ -93,28 +99,26 @@ public class BMIPythonModelFactory implements IModelFactory {
         try {
             //ID of this member
             int instanceID = getNextID();
-            
+
             //we allocate instances to hosts round-robin
             String host = hosts[instanceID % hosts.length];
-            
+
             //create a output directory for this member
             String workDir = String.format("work%02d", instanceID);
             File modelWorkDir = new File(modelRunRootDir, workDir);
             modelWorkDir.mkdirs();
-            
+
             BMIRaster model = LocalPythonThriftBMIRaster.createModel(host, pythonExecutable, bridgeDir, modelDir, modelModule,
-                   modelClass, modelWorkDir);
+                    modelClass, modelWorkDir);
 
             String instanceConfigFile = String.format("%s.%02d", configFile, instanceID);
-            
+
             return new BMIRasterModelInstance(model, modelWorkDir, instanceConfigFile);
         } catch (Exception e) {
             LOGGER.error("failed to create instance", e);
             return null;
         }
     }
-
-  
 
     @Override
     public void finish() {
